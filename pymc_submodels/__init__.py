ef6aa7bc13c30a5ec27c18336c4643aaa442acaa
Daniel Acuna <daniel_acuna@yahoo.com>
2011-06-10 14:34:59 -0500

    Init function in pymc_submodels

---
+++ pymc_submodels/__init__.py
@@ -0,0 +1,7 @@
+'''Common submodels used in Bayesian modeling, implemented in PyMC.'''
+
+from stickbreaking import dp
+
+from gaussian_mixtures import (fixed_mixture_model, infty_mixture_model,  
+            mixture_models)
+
